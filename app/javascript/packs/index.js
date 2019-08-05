785265f53c96a94e77626953c45a4fab59dfe12c
Travis <travisdock@gmail.com>
2019-08-05 06:48:47 -0500

    Add some apollo components

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,1 +1,11 @@
-console.log('👻');
+import React from 'react';
+import { render } from 'react-dom';
+import Provider from '../components/Provider';
+import Library from '../components/Library';
+
+render(
+  <Provider>
+    👻<Library />👻
+  </Provider>,
+  document.querySelector('#root')
+);
