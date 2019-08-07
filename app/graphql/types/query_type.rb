6120daf537fd11e01ebcd1f4e83b5b08380dd2fb
Travis <travisdock@gmail.com>
2019-08-07 06:54:19 -0500

    Add me field to query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,14 +2,17 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-
     field :items,
           [Types::ItemType],
           null: false,
           description: "Returns a list of items in the martian library"
-
     def items
       Item.all
+    end
+
+    field :me, Types::UserType, null: true
+    def me
+      context[:current_user]
     end
   end
 end
