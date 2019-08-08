7ff7eebc54cdb61f9e81a8ce9796532bf82d79f8
Travis <travisdock@gmail.com>
2019-08-08 06:46:20 -0500

    Add UserInfo component

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import Provider from '../components/Provider';
 import Library from '../components/Library';
+import UserInfo from "../components/UserInfo";
 
 render(
   <Provider>
-    👻<Library />👻
+    <UserInfo />
+    <Library />
   </Provider>,
   document.querySelector('#root')
 );
