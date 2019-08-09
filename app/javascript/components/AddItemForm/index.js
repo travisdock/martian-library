3cac25de527d47bdd47f817a13bb41c8d5ac3ab8
Travis <travisdock@gmail.com>
2019-08-09 06:52:28 -0500

    Update AddItem

--- app/javascript/components/AddItemForm/index.js
+++ app/javascript/components/AddItemForm/index.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import cs from './styles';
+import React from "react";
+import { Mutation } from "react-apollo";
+import { AddItemMutation } from "./operations.graphql";
+import ProcessItemForm from "../ProcessItemForm";
+import { LibraryQuery } from '../Library/operations.graphql';
 
 const AddItemForm = () => (
-  <div className={cs.addItemForm}>👋</div>
+  <Mutation mutation={AddItemMutation}>
+    {(addItem, { loading }) => (
+      <ProcessItemForm
+        buttonText="Add Item"
+        loading={loading}
+        // Update library query after Mutation will be finished
+        onProcessItem={({ title, description, imageUrl }) =>
+          addItem({
+            variables: {
+              title,
+              description,
+              imageUrl
+            },
+            // adding the second argument to 'addItem' method
+            update: (cache, { data: { addItem } }) => {
+              const item = addItem.item;
+              if (item) {
+                const currentItems = cache.readQuery({ query: LibraryQuery });
+                cache.writeQuery({
+                  query: LibraryQuery,
+                  data: {
+                    items: [item].concat(currentItems.items),
+                  },
+                });
+              }
+            }
+          })
+        }
+      />
+    )}
+  </Mutation>
 );
 
 export default AddItemForm;
