d8a8055c4fadfa487615ae2029bede6e7b7cb66f
Travis <travisdock@gmail.com>
2019-08-09 06:36:06 -0500

    Update Library component again

--- app/javascript/components/Library/index.js
+++ app/javascript/components/Library/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import cs from './styles';
+import React, { useState } from "react";
+import { Query } from "react-apollo";
+import { LibraryQuery } from "./operations.graphql";
+import cs from "./styles";
 
-const Library = () => (
-  <div className={cs.library}>👋</div>
-);
+const Library = () => {
+  const [item, setItem] = useState(null);
+  return (
+    <Query query={LibraryQuery}>
+      {({ data, loading }) => (
+        <div className={cs.library}>
+          {loading || !data.items
+            ? "loading..."
+            : data.items.map(({ title, id, user, imageUrl, description }) => (
+                <button
+                  key={id}
+                  className={cs.plate}
+                  onClick={() => setItem({ title, imageUrl, id, description })}
+                >
+                  <div className={cs.title}>{title}</div>
+                  <div>{description}</div>
+                  {imageUrl && <img src={imageUrl} className={cs.image} />}
+                  {user ? (
+                    <div className={cs.user}>added by {user.email}</div>
+                  ) : null}
+                </button>
+              ))}
+        </div>
+      )}
+    </Query>
+  );
+};
 
 export default Library;
